4de2ad62b2f52c096716d3807ba2d34550d5f067
Asit das <asit38767@gmail.com>
2023-01-18 21:29:12 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+var canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext("2d");
+
+
+
+function getThing() {
+	var getC = document.getElementById("C").value;
+	var getS = document.getElementById("S").value;
+
+	return {
+		getC, getS
+	};
+}
+
+
+function clearAll(){
+	canvas.width = canvas.width;
+}
+
+function draw(e) {
+	if (drawing == false) return;
+	let editor = getThing();
+	ctx.lineWidth = editor.getS;
+	ctx.strokeStyle = editor.getC;	
+	ctx.lineCap ="round";
+	ctx.lineTo(e.clientX, e.clientY);
+	ctx.moveTo(e.clientX, e.clientY);
+	ctx.stroke();
+
+}
+
+function startDraw(e) {
+	drawing = true;
+	draw(e);
+}
+
+function overDraw() {
+	drawing = false;
+	ctx.beginPath();
+}
+
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mouseup", overDraw);
+canvas.addEventListener("mousemove", draw);
